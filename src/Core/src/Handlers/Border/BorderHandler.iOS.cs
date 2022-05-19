8617f3218ee4b6a862b2814c19b5ca698797b0b4
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2022-05-19 16:47:11 +0200

    Clip the Border content based on the shape

--- src/Core/src/Handlers/Border/BorderHandler.iOS.cs
+++ src/Core/src/Handlers/Border/BorderHandler.iOS.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Maui.Handlers
 			_ = handler.VirtualView ?? throw new InvalidOperationException($"{nameof(VirtualView)} should have been set by base class.");
 			_ = handler.MauiContext ?? throw new InvalidOperationException($"{nameof(MauiContext)} should have been set by base class.");
 
-			//Cleanup the old view when reused
+			// Cleanup the old view when reused
 			var oldChildren = handler.PlatformView.Subviews.ToList();
 			oldChildren.ForEach(x => x.RemoveFromSuperview());
 
